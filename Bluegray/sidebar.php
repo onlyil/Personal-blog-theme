85893ba8da5809d6a5d04c088389dfba003686dd
Life Muze <life@muzerblog.com>
2017-05-03 17:15:29 +0800

    修改sidebar样式
    删除无意义插件

--- Bluegray/sidebar.php
+++ Bluegray/sidebar.php
@@ -20,8 +20,5 @@
             <?php wp_tag_cloud('number=20&orderby=count&order=DESC&smallest=12&largest=16&unit=px'); ?>
         </div>
     </div>
-    <div class="mouse">
-        <object type="application/x-shockwave-flash" style="outline:none;" data="http://cdn.abowman.com/widgets/hamster/hamster.swf?" width="235" height="176"><param name="movie" value="http://cdn.abowman.com/widgets/hamster/hamster.swf?"></param><param name="AllowScriptAccess" value="always"></param><param name="wmode" value="opaque"></param></object>
-    </div>
 </aside>
 
